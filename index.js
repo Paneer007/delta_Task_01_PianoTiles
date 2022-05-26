db180ed64c6b5bcf6bd1ee170ef64f9a9758afe7
Sanjai Kumar <sanjaikumar.mani@gmail.com>
2022-05-26 15:05:52 +0530

    Added regular mode and started hackermode

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const makeGrid =(num)=>{
-    console.log('fshjsdh')
+let round=0;
+let gameMatrix;
+let correct=0;
+let score=0;
+const gameOver=()=>{
+    const root=document.getElementById('root')
+    root.innerHTML=''
+    const gameOverDOM = document.createElement('h1')
+    const scoreDOM = document.createElement('h1')
+    const playAgainDOM = document.createElement('h1')
+    const homeScreenDOM = document.createElement('h1')
+    root.appendChild(gameOverDOM)
+    root.appendChild(scoreDOM)
+    root.appendChild(playAgainDOM)
+    root.appendChild(homeScreenDOM)
+    console.log(score)
+    gameOverDOM.innerText="Game Over"
+    scoreDOM.innerHTML=`your score ${score}`
+    playAgainDOM.innerText='Play Again'
+    homeScreenDOM.innerText='Go Home'
+    playAgainDOM.addEventListener('click',makeNormalMode)
 
+}
+const makeGrid =(num)=>{
     const tilesGrid =document.getElementById('tilesGrid');
     for(var i=0;i<num;i++){
         const rowTile = document.createElement('div')
@@ -10,8 +31,9 @@ const makeGrid =(num)=>{
         for(var j=0;j<num;j++){
             const columnTile =document.createElement('div')
             columnTile.dataset.row=i;
-            columnTile.dataset.columnTile=j;
+            columnTile.dataset.col=j;
             columnTile.className="cell"
+            columnTile.id=`cell ${i} ${j}`
             rowTile.appendChild(columnTile)
         }
     }
@@ -19,52 +41,119 @@ const makeGrid =(num)=>{
 function matrix(m) {
     var result = []
     for(let i = 0; i < m; i++) {
-    console.log('fshjsdh')
-
         result.push(new Array(m).fill(0))
     }
     return result
 }
 const listOfRandomInteger=(num,round)=>{
     console.log(num,round)
-    return
-    let max=round*round
+    let max=num*num
+    console.log(max)
     let arr=[]
-    while(arr.length < num){
-        console.log('fshjsdh')
-        var r = Math.floor(Math.random() * max);
+    while(arr.length < round){
+        let r = Math.floor(Math.random()*max);
         console.log(r,arr)
         if(arr.indexOf(r) === -1) arr.push(r);
     }
     return arr;
 }
-const PlayRound=(num,round)=>{
-    console.log('fshjsdh')
+const resetColor=()=>{
+    const tiles = document.querySelectorAll('.cell');
+    tiles.forEach(x=>x.style.backgroundColor='beige')
+}
+const checkCellNormal=(e)=>{
+    const row= e.target.dataset.row
+    const col = e.target.dataset.col
+    if (gameMatrix[row][col]==-1){
+        null
+    }
+    else if(gameMatrix[row][col]!=0){
+        gameMatrix[row][col]=-1;
+        correct++
+        score++
+    }else{
+        gameOver();
+        return;
+    }
+    if(correct==round){
+        console.log('habibi')
+        correct=0;
+        round++
+        PlayRound(4,round)
+    }
+    UpdateScore()
+}
+const addFunctionality=()=>{
+    const tiles = document.querySelectorAll('.cell');
+    tiles.forEach(x=>x.addEventListener('click',checkCellNormal))
+}
+const ShowColorMatrixAndGameOn= async(randomNumber,num)=>{
+    const cells = document.querySelectorAll('.cell')
+    console.log(cells)
+    cells.forEach(x=>x.removeEventListener('click',checkCellNormal))
+    let timeOut=0
+    for(let i=0;i<randomNumber.length+1;i++){
+        let r=Math.floor(randomNumber[i]/num);
+        let c=randomNumber[i]%num;
+        setTimeout(()=>{
+            if(i==randomNumber.length){
+                resetColor();
+                addFunctionality();
+            }else{
+                resetColor()
+                document.getElementById(`cell ${r} ${c}`).style.backgroundColor="black"
+            }
+        },timeOut+=500)
 
-    let gameMatrix= matrix(num);
+    }
+}
+const PlayRound=async(num,round)=>{
+    gameMatrix= matrix(num);
     const cells = document.querySelectorAll('.cell')
     let randomNumber=listOfRandomInteger(num,round)
     console.log(randomNumber)
-    cells.forEach(x=>x.addEventListener('click',()=>console.log('scoppp')))
+    for(let i=0;i<randomNumber.length;i++){
+        let r=Math.floor(randomNumber[i]/num);
+        let c=randomNumber[i]%num;
+        //for hackermode make sure you index instead of one
+        gameMatrix[r][c]=1;
+    }
+    ShowColorMatrixAndGameOn(randomNumber,num,gameMatrix)
 }
 const playGame=(num)=>{
-    console.log('fshjsdh')
-
-    let round=1
+    round=1
     PlayRound(num,round);
 }
+const addRoundAndScore=()=>{
+    const home=document.getElementById('roundAndScoreDiv')
+    const round= document.createElement('p')
+    const score= document.createElement('p')
+    home.appendChild(round)
+    home.appendChild(score)
+    round.id='roundContent'
+    score.id='scoreContent'
+}
+const UpdateScore=()=>{
+    document.getElementById('roundContent').innerText=`round: ${round}`
+    document.getElementById('scoreContent').innerText=`score: ${score}`
+
+}
 const makeNormalMode=()=>{
-    console.log('fshjsdh')
+    round=0
+    correct=0
+    score=0
     const root=document.getElementById('root')
     root.innerHTML=''
+    const roundAnddScoreDiv=document.createElement('div')
     const gameDiv = document.createElement('div')
+    root.appendChild(roundAnddScoreDiv)
     root.appendChild(gameDiv)
     gameDiv.id="tilesGrid"
-    let score=0
+    roundAnddScoreDiv.id="roundAndScoreDiv"
+    addRoundAndScore()
+    UpdateScore()
     makeGrid(4)
     playGame(4)
-
-
 }
 const setHomePage=()=>{
     const button = document.getElementById('normalModeButton');
@@ -73,7 +162,26 @@ const setHomePage=()=>{
         makeNormalMode()
     })
 }
-
+const homeMenuLayout=()=>{
+    const gameNameDiv = document.createElement('div')
+    const modesDiv = document.createElement('div')
+    const PianoTileGame= document.createElement('h1')
+    const GameModes= document.createElement('h2')
+    const NormalMode= document.createElement('p')
+    const HackerMode= document.createElement('p')
+    const HackerppMode= document.createElement('p')
+    const buttonListDiv = document.createElement('div')
+    const rootDiv = document.getElementById('root')
+    rootDiv.innerHTML=''
+    rootDiv.appendChild(gameNameDiv)
+    rootDiv.appendChild(modesDiv)
+    modesDiv.appendChild(GameModes)
+    modesDiv.appendChild(buttonListDiv)
+    buttonListDiv.appendChild(NormalMode)
+    buttonListDiv.appendChild(HackerMode)
+    buttonListDiv.appendChild(HackerppMode)
+    gameNameDiv.appendChild(PianoTileGame)
+}
 function main(){
     setHomePage()
 }
